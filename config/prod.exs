97b71e13a0e2d932224e6d84621bd6e535b2a8e3
Guillaume Malette <guillaume@shopify.com>
2017-02-04 22:55:00 -0500

    Fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :doucheracer, Doucheracer.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "doucherace.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "doucheracer.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
